13967fa4548d096f447dd12f6a9e94ddbc91f7a7
CXX-241211 <969634474@qq.com>
2025-05-14 13:55:50 +0800

    优化

--- a_Actor/NPC/level_1/lion.gd
+++ a_Actor/NPC/level_1/lion.gd
@@ -1,13 +1,1 @@
 extends CharacterBody2D
-
-
-if velocity.x > 0:
-	$BoySprite.play('move_right')
-elif velocity.x < 0:
-	$BoySprite.play('move_left')
-elif velocity.y > 0:
-	$BoySprite.play('move_down')
-elif velocity.y < 0:
-	$BoySprite.play('move_up')
-else:
-	$BoySprite.stop()
